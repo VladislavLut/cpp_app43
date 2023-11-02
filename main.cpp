58f41191dc9c952e8e4695961b4332f5eb47dc53
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-11-02 20:27:33 +0200

    ex-2 was done

--- main.cpp
+++ main.cpp
@@ -1,78 +1,89 @@
 #include <iostream>
-#include <algorithm>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-const int SIZE = 4; 
-
-template <typename T>
-void initializeMatrix(T matrix[][SIZE], int size) {
+int findMax(const int arr[], int size) {
+    int max = INT_MIN;
     for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            matrix[i][j] = static_cast<T>(rand() % 100);
+        if (arr[i] > max) {
+            max = arr[i];
         }
     }
+    return max;
 }
 
-template <typename T>
-void printMatrix(const T matrix[][SIZE], int size) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            cout << matrix[i][j] << " ";
+int findMax(const int arr[][3], int rows) {
+    int max = INT_MIN;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
         }
-        cout << endl;
     }
+    return max;
 }
 
-template <typename T>
-void findDiagonalMinMax(const T matrix[][SIZE], int size) {
-    T max = matrix[0][0];
-    T min = matrix[0][0];
-
-    for (int i = 1; i < size; ++i) {
-        if (matrix[i][i] > max) {
-            max = matrix[i][i];
-        }
-        if (matrix[i][i] < min) {
-            min = matrix[i][i];
+int findMax(const int arr[][3][2], int rows) {
+    int max = INT_MIN;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                if (arr[i][j][k] > max) {
+                    max = arr[i][j][k];
+                }
+            }
         }
     }
-
-    cout << "Max on the main diagonal: " << max << endl;
-    cout << "Min on the main diagonal: " << min << endl;
+    return max;
 }
 
-template <typename T>
-void sortRowsAscending(T matrix[][SIZE], int size) {
-    for (int i = 0; i < size; ++i) {
-        std::sort(matrix[i], matrix[i] + size);
-    }
+int findMax(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int findMax(int a, int b, int c) {
+    return findMax(findMax(a, b), c);
 }
 
 int main() {
-    srand(static_cast<unsigned int>(time(0)));
+    srand(time(0));
 
-    int intMatrix[SIZE][SIZE];
-    double doubleMatrix[SIZE][SIZE];
+    int oneDim[5];
+    int twoDim[2][3];
+    int threeDim[2][3][2];
 
-    initializeMatrix(intMatrix, SIZE);
-    initializeMatrix(doubleMatrix, SIZE);
+    for (int i = 0; i < 5; ++i) {
+        oneDim[i] = rand() % 100;
+    }
 
-    cout << "Initialized integer matrix:" << endl;
-    printMatrix(intMatrix, SIZE);
-    findDiagonalMinMax(intMatrix, SIZE);
-    sortRowsAscending(intMatrix, SIZE);
-    cout << "Integer matrix with rows sorted in ascending order:" << endl;
-    printMatrix(intMatrix, SIZE);
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            twoDim[i][j] = rand() % 100;
+        }
+    }
 
-    cout << "Initialized double matrix:" << endl;
-    printMatrix(doubleMatrix, SIZE);
-    findDiagonalMinMax(doubleMatrix, SIZE);
-    sortRowsAscending(doubleMatrix, SIZE);
-    cout << "Double matrix with rows sorted in ascending order:" << endl;
-    printMatrix(doubleMatrix, SIZE);
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                threeDim[i][j][k] = rand() % 100;
+            }
+        }
+    }
+
+    int maxOneDim = findMax(oneDim, 5);
+    int maxTwoDim = findMax(twoDim, 2);
+    int maxThreeDim = findMax(threeDim, 2);
+    int maxTwoInt = findMax(10, 30);
+    int maxThreeInt = findMax(20, 50, 30);
+
+    cout << "Max in one-dimensional array: " << maxOneDim << endl;
+    cout << "Max in two-dimensional array: " << maxTwoDim << endl;
+    cout << "Max in three-dimensional array: " << maxThreeDim << endl;
+    cout << "Max of two integers: " << maxTwoInt << endl;
+    cout << "Max of three integers: " << maxThreeInt << endl;
 
     return 0;
 }
