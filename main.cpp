3abe8cb58f70c9a67c0107e6d5776df95c057443
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-11-02 20:33:16 +0200

    ex-2 was done

--- main.cpp
+++ main.cpp
@@ -1,37 +1,58 @@
 #include "Header.h"
 
-
 using namespace std;
+
+template<typename T>
+void reverseArray(T arr[], size_t size) {
+    size_t start = 0;
+    size_t end = size - 1;
+
+    while (start < end) {
+        T temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        ++start;
+        --end;
+    }
+}
 
 int main() {
     srand(static_cast<unsigned>(time(0)));
 
-    const size_t INT_ARRAY_SIZE = 6;
-    const size_t DOUBLE_ARRAY_SIZE = 5;
+    int intArray[] = { rand() % 100, rand() % 100, rand() % 100, rand() % 100, rand() % 100 };
+    double doubleArray[] = { static_cast<double>(rand() % 100) / 10.0, static_cast<double>(rand() % 100) / 10.0,
+                            static_cast<double>(rand() % 100) / 10.0, static_cast<double>(rand() % 100) / 10.0,
+                            static_cast<double>(rand() % 100) / 10.0 };
 
-    int intArray[INT_ARRAY_SIZE];
-    double doubleArray[DOUBLE_ARRAY_SIZE];
+    const size_t INT_ARRAY_SIZE = sizeof(intArray) / sizeof(intArray[0]);
+    const size_t DOUBLE_ARRAY_SIZE = sizeof(doubleArray) / sizeof(doubleArray[0]);
 
+    cout << "Original integer array: ";
     for (size_t i = 0; i < INT_ARRAY_SIZE; ++i) {
-        intArray[i] = rand() % 100;
+        cout << intArray[i] << " ";
     }
+    cout << endl;
 
+    cout << "Original double array: ";
     for (size_t i = 0; i < DOUBLE_ARRAY_SIZE; ++i) {
-        doubleArray[i] = (rand() % 1000) / 10.0;
+        cout << doubleArray[i] << " ";
     }
+    cout << endl;
 
-    int intMinValue, intMaxValue;
-    double doubleMinValue, doubleMaxValue;
-    size_t intMinIndex, intMaxIndex, doubleMinIndex, doubleMaxIndex;
+    reverseArray(intArray, INT_ARRAY_SIZE);
+    reverseArray(doubleArray, DOUBLE_ARRAY_SIZE);
 
-    findMinMax(intArray, INT_ARRAY_SIZE, intMinValue, intMaxValue, intMinIndex, intMaxIndex);
-    findMinMax(doubleArray, DOUBLE_ARRAY_SIZE, doubleMinValue, doubleMaxValue, doubleMinIndex, doubleMaxIndex);
+    cout << "Reversed integer array: ";
+    for (size_t i = 0; i < INT_ARRAY_SIZE; ++i) {
+        cout << intArray[i] << " ";
+    }
+    cout << endl;
 
-    cout << "For the integer array: Min = " << intMinValue << " at index " << intMinIndex
-        << ", Max = " << intMaxValue << " at index " << intMaxIndex << endl;
-
-    cout << "For the double array: Min = " << doubleMinValue << " at index " << doubleMinIndex
-        << ", Max = " << doubleMaxValue << " at index " << doubleMaxIndex << endl;
+    cout << "Reversed double array: ";
+    for (size_t i = 0; i < DOUBLE_ARRAY_SIZE; ++i) {
+        cout << doubleArray[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
